3a34b496ba212010a169d99dc472225c17a27bf2
HixRaid <qqwweerrttsava@gmail.com>
2024-02-12 22:55:41 +0300

    Added survey scene

---
+++ game/scripts/interfaces/survey_interface.gd
@@ -0,0 +1,20 @@
+extends "res://game/scripts/interfaces/interface.gd"
+
+
+func _on_restart():
+	pass
+
+func _on_yes():
+	pass
+
+func _on_no():
+	pass
+
+func _on_more_likely_yes():
+	pass
+
+func _on_more_likely_no():
+	pass
+
+func _on_do_not_know():
+	pass
---
+++ game/scripts/survey/survey_data.gd
@@ -0,0 +1,56 @@
+extends Node
+
+var traits = [
+	"Trait 1", "Trait 2"
+]
+
+var characters = [
+	{
+		"name": "Character 1",
+		"description": "Description 1",
+		"traits": [0.5, 0.9]
+	},
+	{
+		"name": "Character 2",
+		"description": "Description 2",
+		"traits": [0.1, 0.2]
+	},
+	{
+		"name": "Character 3",
+		"description": "Description 3",
+		"traits": [0.6, 0.5]
+	}
+]
+
+var questions = [
+	{
+		"question": "Question 1",
+		"positive": false,
+		"trait": 0
+	},
+	{
+		"question": "Question 2",
+		"positive": true,
+		"trait": 1
+	},
+	{
+		"question": "Question 3",
+		"positive": true,
+		"trait": 0
+	},
+	{
+		"question": "Question 4",
+		"positive": false,
+		"trait": 1
+	},
+	{
+		"question": "Question 5",
+		"positive": true,
+		"trait": 0
+	},
+	{
+		"question": "Question 6",
+		"positive": true,
+		"trait": 1
+	}
+]
