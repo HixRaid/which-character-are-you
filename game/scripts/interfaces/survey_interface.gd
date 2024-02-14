09528f8e9e86b4141e7e0782eca2e0714e7e6c59
HixRaid <qqwweerrttsava@gmail.com>
2024-02-14 23:15:10 +0300

    Added survey

--- game/scripts/interfaces/survey_interface.gd
+++ game/scripts/interfaces/survey_interface.gd
@@ -1,25 +1,53 @@
 extends "res://game/scripts/interfaces/interface.gd"
 
+onready var progress_bar = $Panel/ProgressBar
+onready var question_label = $Panel/QuestionLabel
+onready var do_not_know_label = $Panel/Answers/DoNotKnowPanel/Label
+onready var do_not_know_button = $Panel/Answers/DoNotKnowPanel/Button
+onready var interface_switch = $"%InterfaceSwitch"
+
+func _ready():
+	var _error = Survey.connect("finished", self, "_on_finished")
+	Survey.start()
+	progress_bar.max_value = len(Survey.data.QUESTIONS)
+	update_data()
+
+
+func update_data():
+	var question = Survey.get_next_question()
+	question_label.text = question.question
+	progress_bar.value = Survey.current_survey.passed
+
 
 func _on_restart():
-	pass
+	Survey.start()
+	update_data()
 
 
 func _on_yes():
-	pass
+	Survey.vote(Survey.YES)
+	update_data()
 
 
 func _on_no():
-	pass
+	Survey.vote(Survey.NO)
+	update_data()
 
 
 func _on_more_likely_yes():
-	pass
+	Survey.vote(Survey.MORE_LIKELY_YES)
+	update_data()
 
 
 func _on_more_likely_no():
-	pass
+	Survey.vote(Survey.MORE_LIKELY_NO)
+	update_data()
 
 
 func _on_do_not_know():
-	pass
+	Survey.vote(Survey.DO_NOT_KNOW)
+	update_data()
+
+
+func _on_finished():
+	interface_switch.set_visible_interface("Results")
--- game/scripts/survey/survey.gd
+++ game/scripts/survey/survey.gd
@@ -1,3 +1,62 @@
 extends Node
 
+signal finished()
+
+enum {
+	YES = 1,
+	NO = 2,
+	MORE_LIKELY_YES = 3,
+	MORE_LIKELY_NO = 4,
+	DO_NOT_KNOW = 5
+}
+
+var current_survey = {
+	"traits": [],
+	"passed": 0
+}
+
 onready var data = $Data
+
+
+func start():
+	current_survey.traits.clear()
+	current_survey.passed = 0
+	
+	for i in data.TRAITS:
+		current_survey.traits.append({
+			"name": i,
+			"value": 0,
+			"count": 0,
+		})
+
+
+func vote(vote):
+	var question = data.QUESTIONS[current_survey.passed]
+	current_survey.passed += 1
+	
+	match vote:
+		1:
+			add_vote(1, question)
+		2:
+			add_vote(-1, question)
+		3:
+			add_vote(0.5, question)
+		4:
+			add_vote(-0.5, question)
+	
+	if current_survey.passed == len(data.QUESTIONS):
+		emit_signal("finished")
+		start()
+
+
+func add_vote(value, question):
+	if question.positive:
+		current_survey.traits[question.trait].value += value
+	else:
+		current_survey.traits[question.trait].value -= value
+	
+	current_survey.traits[question.trait].count += 1
+
+
+func get_next_question():
+	return data.QUESTIONS[current_survey.passed]
