72e5541b4a9e6c4b96d70324b407257467874aa9
HixRaid <qqwweerrttsava@gmail.com>
2024-03-27 22:16:47 +0300

    Added translation

--- game/scripts/interfaces/settings_interface.gd
+++ game/scripts/interfaces/settings_interface.gd
@@ -1,5 +1,26 @@
 extends "res://game/scripts/interfaces/interface.gd"
 
+onready var language_option_button = $Panel/LanguagePanel/OptionButton
+
+
+func _ready():
+	match OS.get_locale_language():
+		"en":
+			language_option_button.selected = 0
+		"ru":
+			language_option_button.selected = 1
+		"tr":
+			language_option_button.selected = 2
+
+
+func _on_language_selected(index):
+	match index:
+		0:
+			TranslationServer.set_locale("en")
+		1:
+			TranslationServer.set_locale("ru")
+		2:
+			TranslationServer.set_locale("tr")
 
 func _on_scale_selected(index):
 	match index:
