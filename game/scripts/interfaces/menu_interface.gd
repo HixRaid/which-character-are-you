c66ef59bda6ca9823d3455c458cb9fe617d12b1a
HixRaid <qqwweerrttsava@gmail.com>
2024-04-20 21:29:15 +0300

    Removed quit button for web

--- game/scripts/interfaces/menu_interface.gd
+++ game/scripts/interfaces/menu_interface.gd
@@ -1,5 +1,12 @@
 extends "res://game/scripts/interfaces/interface.gd"
 
+onready var quit_button = $Panel/QuitButton
+
+
+func _ready():
+	if OS.has_feature("web"):
+		quit_button.visible = false
+
 
 func quit():
 	get_tree().quit()
